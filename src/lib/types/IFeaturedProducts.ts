d5d7b85565c16d6b50c879f6d4552da538a2627a
Joel Varty <joel@agilitycms.com>
2025-11-10 09:27:26 -0500

    fix: Update IFeaturedProducts interface to use URLField for ctaLink

--- src/lib/types/IFeaturedProducts.ts
+++ src/lib/types/IFeaturedProducts.ts
@@ -1,10 +1,10 @@
-import type { ContentItem, ImageField, LinkField } from "@agility/nextjs"
+import type { ContentItem, ImageField, URLField } from "@agility/nextjs"
 import type { IProduct } from "./IProduct"
 
 export interface IFeaturedProducts {
   heading: string
   ctaText: string
-  ctaLink: LinkField
+  ctaLink: URLField
   featuredProducts: ContentItem<IProduct>[] // Auto-populated by SDK for search list box
   productIDs: string
   productTitles: string
